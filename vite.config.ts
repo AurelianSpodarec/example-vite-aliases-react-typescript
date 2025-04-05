128aeef1aaca6d724f40fe741298031d07ae02de
aurelian.spod@gmail.com <aurelian.spod@gmail.com>
2025-04-05 02:22:42 +0200

    chore: add path alias support, install path package, and update imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@components": path.resolve('./src/components'),
+      "@atoms": path.resolve('./src/components/atoms'),
+      "@assets": path.resolve('./src/assets'),
+      "@": path.resolve("./src"),
+    },
+  },
 })
